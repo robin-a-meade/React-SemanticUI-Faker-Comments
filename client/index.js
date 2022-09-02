4e33ec25a37d7bbcea65f7c4d9f4ce67ee6a6994
Robin A. Meade <robin.a.meade@gmail.com>
2022-09-02 03:10:00 +0000

    Add some example markup

--- client/index.js
+++ client/index.js
@@ -1,5 +1,30 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
+const App = () => {
+  return (
+    <div className="ui comments">
+      <h3 className="ui dividing header">Comments</h3>
+      <div className="comment">
+        <a className="avatar">
+          <img src="https://semantic-ui.com/images/avatar/small/matt.jpg" />
+        </a>
+        <div className="content">
+          <a className="author">Matt</a>
+          <div className="metadata">
+            <span className="date">Today at 5:42PM</span>
+          </div>
+          <div className="text">
+            How artistic!
+          </div>
+          <div className="actions">
+            <a className="reply">Reply</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello from React component!</h1>);
+root.render(<App />);
