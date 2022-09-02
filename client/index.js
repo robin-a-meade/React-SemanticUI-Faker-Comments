53f239616f949c7efcd132c4b1664217c5f25c44
Robin A. Meade <robin.a.meade@gmail.com>
2022-09-02 07:20:39 +0000

    Introduce faker

--- client/index.js
+++ client/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { faker } from "@faker-js/faker";
+
+// TODO:
+// For each comment, generate a random timestamps using faker
+// See: https://fakerjs.dev/api/date.html
+//
+// Format the generated random timestamp relatively
+// See: https://bobbyhadz.com/blog/javascript-convert-timestamp-to-time-ago)
+// Leads to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat/RelativeTimeFormat
+
 
 const App = () => {
   return (
@@ -7,7 +17,7 @@ const App = () => {
       <h3 className="ui dividing header">Comments</h3>
       <div className="comment">
         <a className="avatar">
-          <img src="https://semantic-ui.com/images/avatar/small/matt.jpg" />
+          <img src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a className="author">Matt</a>
@@ -15,7 +25,7 @@ const App = () => {
             <span className="date">Today at 5:42PM</span>
           </div>
           <div className="text">
-            How artistic!
+            {faker.hacker.phrase()}
           </div>
           <div className="actions">
             <a className="reply">Reply</a>
