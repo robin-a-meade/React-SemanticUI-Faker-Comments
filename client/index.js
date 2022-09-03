24795a9defe40d617d850409685d221029d10c9e
Robin A. Meade <robin.a.meade@gmail.com>
2022-09-03 03:09:54 +0000

    Try fake date

--- client/index.js
+++ client/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { faker } from "@faker-js/faker";
+//import moment from 'moment';
 
 // TODO:
-// For each comment, generate a random timestamps using faker
+// For each comment, generate a random timestamp using faker
 // See: https://fakerjs.dev/api/date.html
 //
 // Format the generated random timestamp relatively
 // See: https://bobbyhadz.com/blog/javascript-convert-timestamp-to-time-ago)
 // Leads to: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/RelativeTimeFormat/RelativeTimeFormat
 
+const rtf = new Intl.RelativeTimeFormat('en', {
+  numeric: 'auto',
+});
 
 const App = () => {
+  //console.log(faker.date.recent(10));
   return (
     <div className="ui comments">
       <h3 className="ui dividing header">Comments</h3>
@@ -20,9 +25,9 @@ const App = () => {
           <img src={faker.image.avatar()} />
         </a>
         <div className="content">
-          <a className="author">Matt</a>
+          <a className="author">{faker.name.firstName()}</a>
           <div className="metadata">
-            <span className="date">Today at 5:42PM</span>
+            <span className="date">{faker.date.recent(10).toString()}</span>
           </div>
           <div className="text">
             {faker.hacker.phrase()}
