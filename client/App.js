58cf068a05aaf97f99c4cdede015ef607807c954
Robin A. Meade <robin.a.meade@gmail.com>
2022-09-04 22:03:59 +0000

    Add ApprovalCard and style.css

--- client/App.js
+++ client/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "./api";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   const [items, setItems] = React.useState([]);
@@ -15,17 +16,20 @@ const App = () => {
   return (
     <div className="ui comments">
       <h3 className="ui dividing header">Comments</h3>
-      {items.map((item, index) => {
-        return (
-          <CommentDetail
-            key={index}
-            name={item.name}
-            avatar={item.avatar}
-            date={item.date}
-            text={item.text}
-          />
-        );
-      })}
+      <div className="ui cards">
+        {items.map((item, index) => {
+          return (
+            <ApprovalCard key={index}>
+              <CommentDetail
+                name={item.name}
+                avatar={item.avatar}
+                date={item.date}
+                text={item.text}
+              />
+            </ApprovalCard>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import api from "./api";
 import App from "./App";
+import './style/App.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
